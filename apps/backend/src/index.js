f073e0d101617f669f25ebf4ab746606c29eb221
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-08-08 13:36:37 +0800

    feat: add cors

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,11 +1,17 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
 const PORT = 3000
 
-app.get("/", (req, res) => {
-    res.send("Hello from Backend")
-})
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+app.use(express.json())
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from backend API!" });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port:${PORT}`)
